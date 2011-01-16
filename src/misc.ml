d068e8f8fb490ae8083702d40111785094d4b59d
Taro Sekiyama <t-sekiym@kuis.kyoto-u.ac.jp>
2011-01-17 04:16:05 +0900

    add a function

--- src/misc.ml
+++ src/misc.ml
@@ -1,3 +1,10 @@
 let (@<) = fun x y -> x y
 let (+>) = fun x y -> y x
 let ($<)  = fun f g x -> f (g x)
+
+let make_list i x =
+  let rec iter ii acc =
+    if ii = 0 then acc
+    else iter (ii-1) (x::acc)
+  in
+  iter i []
