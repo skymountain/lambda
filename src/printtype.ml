77b32e37b66fc2fe1640059cb8fa850d3729e061
Taro Sekiyama <t-sekiym@kuis.kyoto-u.ac.jp>
2011-01-17 04:16:23 +0900

    use template for pretty printer

--- src/printtype.ml
+++ src/printtype.ml
@@ -28,43 +28,58 @@ let new_typvar typvar =
   iter (String.copy typvar) (len - 1)
 
 (* pretty printer for types *)
-let pps_typvar typvar_map typvar id =
+let pps_typvar id typvar_map typvar =
   try (typvar_map, typvar, TypvarMap.find id typvar_map) with
     Not_found -> (TypvarMap.add id typvar typvar_map, new_typvar typvar, typvar)
 
 let map_with_paren = List.map
   (fun (with_paren, s) -> if with_paren then Printf.sprintf "(%s)" s else s)
 
+(* replace %s with exact string and %p with string with parenthesis *)
+let pps_template template fs tvmap typvar is_bound with_paren =
+  let tvmap, typvar, xs =
+    List.fold_left (fun (tvmap, typvar, xs) f ->
+                      let tvmap, typvar, with_paren, s = f tvmap typvar is_bound in
+                      (tvmap, typvar, (with_paren, s)::xs))
+      (tvmap, typvar, []) fs
+  in
+  let xs = List.rev xs in
+  let pre = ref 0 in
+  let ret = ref "" in
+  List.iter (fun (with_paren, to_str) ->
+               let pos = String.index_from template !pre '%' in
+               assert (pos + 1 < String.length template);
+               let to_str =
+                 if not with_paren then to_str
+                 else match String.get template @< pos + 1 with
+                 | 's' -> to_str
+                 | 'p' -> Printf.sprintf "(%s)" to_str
+                 | _   -> assert false
+               in
+               ret := !ret ^ (String.sub template !pre (pos - !pre)) ^ to_str;
+               pre := pos + 2)
+    xs;
+  (tvmap, typvar, with_paren, !ret)
+
 let pps =
-  let rec pps_inner tvmap typvar is_bound = function
+  let rec pps_inner typ tvmap typvar is_bound = match typ with
     | TyFun (typ1, typ2) -> begin
-        let tvmap, typvar, with_paren, t1 = pps_inner tvmap typvar is_bound typ1 in
-        let t1 = if with_paren then "("^t1^")" else t1 in
-        let tvmap, typvar, _, t2 = pps_inner tvmap typvar is_bound typ2 in
-        (tvmap, typvar, true, t1^" -> "^t2)
+        pps_template "%p -> %s" [(pps_inner typ1); (pps_inner typ2)] tvmap typvar is_bound true
       end
     | TyVar tv -> begin
-        let tvmap, typvar, s = pps_typvar tvmap typvar tv in
+        let tvmap, typvar, s = pps_typvar tv tvmap typvar in
         let s = "'" ^ (if is_bound tv then "" else "_") ^ s in
         (tvmap, typvar, false, s)
       end
     | TyVariant (typs, ident) | TyAlias (_, typs, ident) -> begin
-        let tvmap, typvar, xs = pps_typ_list_inner tvmap typvar is_bound typs in
-        let params_str = map_with_paren xs +> String.concat " " in
-        let typ_name = Ident.name ident in
-        if String.length params_str = 0 then (tvmap, typvar, false, typ_name)
-        else (tvmap, typvar, false, Printf.sprintf "%s %s" params_str typ_name)
+        let template = make_list (List.length typs) "%p " +> String.concat "" +> (fun s -> s^"%s") in
+        let fs = List.fold_right (fun typ acc -> (pps_inner typ)::acc)
+          typs [(fun tvmap typvar _ -> (tvmap, typvar, false, Ident.name ident))]
+        in
+        pps_template template fs tvmap typvar is_bound false
       end
-  and pps_typ_list_inner tvmap typvar is_bound typs =
-    let tvmap, typvar, xs =
-      List.fold_left (fun (tvmap, typvar, acc) typ ->
-                        let tvmap, typvar, with_paren, s = pps_inner tvmap typvar is_bound typ in
-                        (tvmap, typvar, (with_paren, s)::acc))
-        (tvmap, typvar, []) typs
-    in
-    (tvmap, typvar, List.rev xs)
   in
-  (fun is_bound typ -> let _, _, _, s = pps_inner TypvarMap.empty "a" is_bound typ in s)
+  (fun is_bound typ -> let _, _, _, s = pps_inner typ TypvarMap.empty "a" is_bound in s)
 
 let pps_typ = pps (fun _ -> true)
 let pp_typ typ = print_string @< pps_typ typ
