67820f402e8f307a06d014cf865ed2ef32b0cdb1
Taro Sekiyama <t-sekiym@kuis.kyoto-u.ac.jp>
2011-01-17 03:29:29 +0900

    modify to return whether need parenthesis

--- src/printtype.ml
+++ src/printtype.ml
@@ -28,53 +28,43 @@ let new_typvar typvar =
   iter (String.copy typvar) (len - 1)
 
 (* pretty printer for types *)
-let is_funtyp_without_paren s =
-  let len = String.length s in
-  let rec iter idx depth =
-    if idx >= len then false
-    else match s.[idx] with
-      '(' -> iter (idx+1) (depth+1)
-    | ')' -> iter (idx+1) (depth-1)
-    | '-' when idx+1 < len && s.[idx+1] = '>'-> true
-    | _   -> iter (idx+1) depth
-  in
-  iter 0 0
-
 let pps_typvar typvar_map typvar id =
   try (typvar_map, typvar, TypvarMap.find id typvar_map) with
     Not_found -> (TypvarMap.add id typvar typvar_map, new_typvar typvar, typvar)
 
+let map_with_paren = List.map
+  (fun (with_paren, s) -> if with_paren then Printf.sprintf "(%s)" s else s)
+
 let pps =
   let rec pps_inner tvmap typvar is_bound = function
     | TyFun (typ1, typ2) -> begin
-        let tvmap, typvar, t1 = pps_inner tvmap typvar is_bound typ1 in
-        let t1 = if is_funtyp_without_paren t1 then "("^t1^")" else t1 in
-        let tvmap, typvar, t2 = pps_inner tvmap typvar is_bound typ2 in
-        (tvmap, typvar, t1^" -> "^t2)
+        let tvmap, typvar, with_paren, t1 = pps_inner tvmap typvar is_bound typ1 in
+        let t1 = if with_paren then "("^t1^")" else t1 in
+        let tvmap, typvar, _, t2 = pps_inner tvmap typvar is_bound typ2 in
+        (tvmap, typvar, true, t1^" -> "^t2)
       end
     | TyVar tv -> begin
         let tvmap, typvar, s = pps_typvar tvmap typvar tv in
         let s = "'" ^ (if is_bound tv then "" else "_") ^ s in
-        (tvmap, typvar, s)
+        (tvmap, typvar, false, s)
       end
     | TyVariant (typs, ident) | TyAlias (_, typs, ident) -> begin
-        let tvmap, typvar, typ_strs = pps_typ_list_inner tvmap typvar is_bound typs in
-        let params_str = String.concat " " typ_strs in
+        let tvmap, typvar, xs = pps_typ_list_inner tvmap typvar is_bound typs in
+        let params_str = map_with_paren xs +> String.concat " " in
         let typ_name = Ident.name ident in
-        if String.length params_str = 0 then (tvmap, typvar, typ_name)
-        else (tvmap, typvar, Printf.sprintf "%s %s" params_str typ_name)
+        if String.length params_str = 0 then (tvmap, typvar, false, typ_name)
+        else (tvmap, typvar, false, Printf.sprintf "%s %s" params_str typ_name)
       end
   and pps_typ_list_inner tvmap typvar is_bound typs =
-    let tvmap, typvar, typ_strs =
+    let tvmap, typvar, xs =
       List.fold_left (fun (tvmap, typvar, acc) typ ->
-                        let tvmap, typvar, s = pps_inner tvmap typvar is_bound typ in
-                        (tvmap, typvar, s::acc))
+                        let tvmap, typvar, with_paren, s = pps_inner tvmap typvar is_bound typ in
+                        (tvmap, typvar, (with_paren, s)::acc))
         (tvmap, typvar, []) typs
     in
-    (tvmap, typvar, List.rev typ_strs)
+    (tvmap, typvar, List.rev xs)
   in
-  (fun is_bound typ -> let _, _, s = pps_inner TypvarMap.empty "a" is_bound typ in s)
-
+  (fun is_bound typ -> let _, _, _, s = pps_inner TypvarMap.empty "a" is_bound typ in s)
 
 let pps_typ = pps (fun _ -> true)
 let pp_typ typ = print_string @< pps_typ typ
